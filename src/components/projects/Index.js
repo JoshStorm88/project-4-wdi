582daf91a519012466c1eb90887a4a9a8c553be5
dcravenor <happy-james@hotmail.co.uk>
2018-07-13 11:29:07 +0100

    added project index to help bug fix

--- src/components/projects/Index.js
+++ src/components/projects/Index.js
@@ -1,14 +1,42 @@
 import React from 'react';
-
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+// import _ from 'lodash';
 
 
 class ProjectsIndex extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    };
+  }
+
+  componentDidMount() {
+    axios({
+      url: '/api/projects',
+      method: 'GET'
+    })
+      .then(res => this.setState({ projects: res.data}));
+  }
 
   render() {
     return (
-      <div className="column">
-
+      <div className="columns is-multiline">
+        {this.state.projects.map(project =>
+          <div key={project._id} className="column is-one-third-desktop is-half-tablet">
+            <Link to={`/projects/${project._id}`}>
+              <div className="card">
+                <div className="card-content">
+                  <div className="content">
+                    <h2 className="title">{project.projectName}</h2>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        )}
       </div>
     );
   }
