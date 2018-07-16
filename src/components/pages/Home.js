9ff5b9101b3f6e6c25ed3f924fc5015ce5d05c57
Josh Storm <joshua_taljaard@outlook.com>
2018-07-16 11:09:59 +0100

    minor styling changes to homepage title, subtitle

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
     <section className="hero is-large is-tablet is-mobile">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title">
+          <h1 className="homeTitle">
             Four Horsemen Development Platform
           </h1>
-          <h2 className="subtitle">
+          <h2 className="homeSubtitle">
             No Design experience? Its not the end of the world.
             <div className="text-wrapper">We are&nbsp;
               <div className="animated-words">
--- src/components/projects/Show.js
+++ src/components/projects/Show.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import Auth from '../../lib/Auth';
 
 class ProjectsShow extends React.Component {
