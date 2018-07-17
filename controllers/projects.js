baf6c5525a951e983126ed12b234c667a60cbb42
dcravenor <happy-james@hotmail.co.uk>
2018-07-17 10:00:39 +0100

    added data to tests

--- controllers/projects.js
+++ controllers/projects.js
@@ -17,7 +17,7 @@ function submitRoute() {
   Emailer.sendMail({
     to: 'codeshoptest@gmail.com',
     subject: 'New Project',
-    text: 'Hey, have a look at this awesome project'
+    text: 'Hey, have a look at this awesome project we have for you, the details are as follows......Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   });
 }
 
