1bf7c35435f8f5674de98f8199fb3aba60771dae
Josh Storm <joshua_taljaard@outlook.com>
2018-07-18 10:56:18 +0100

    minor styling changes on Homepage and added flashmessage on send new project

--- src/components/developers/Show.js
+++ src/components/developers/Show.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 // import Auth from '../../lib/Auth';
 import GoogleMap from '../common/GoogleMap';
+import Footer from '../common/Footer';
 
 
 class DevelopersShow extends React.Component {
@@ -27,34 +28,49 @@ class DevelopersShow extends React.Component {
         <div className="column is-one-third">
           <figure className="image is-square">
             <img className="devImage" src={this.state.developer.image} />
-
           </figure>
           < hr />
           <Link className="button is-primary" to={'/projects/new'}>Contact this developer about a project</Link>
+
+          <p className="">See below for {this.state.developer.companyName} most recent projects</p> 
+
+          <img className="devPortfolio" src="https://richwp.com/wp-content/uploads/2013/07/handsome-portfolio-website.jpg"
+            alt="Four Horsemen Development Platform"/>
+
+          <img className="devPortfolio" src="http://cdn.instantshift.com/wp-content/uploads/2012/04/facpwdfi-04.jpg"
+            alt="Four Horsemen Development Platform" />
+
+          <img className="devPortfolio" src="http://www.onextrapixel.com/wp-content/uploads/2015/06/6-NewsTech.jpg"
+            alt="Four Horsemen Development Platform" />
+
         </div>
         <div className="column is-two-thirds">
 
-          <h1 className="title">Company Name: {this.state.developer.companyName}</h1>
+          <h1 className="title">Company Name: <p>{this.state.developer.companyName}</p></h1>
           <hr />
 
-          <h3 className="title">Company Size: {this.state.developer.companySize}</h3>
+          <h1 className="title">Company Size: <p>{this.state.developer.companySize}</p></h1>
           <hr />
 
-          <h1 className="title">Description:<p>{this.state.developer.description}</p></h1>
+          <h1 className="title">Description:</h1><p>{this.state.developer.description}</p>
           <hr />
 
-          <h3 className="title">Skills: {this.state.developer.skills}</h3>
+          <h1 className="title">Skills: <p className="skillsColor">{this.state.developer.skills}</p></h1>
           <hr />
 
-          <h3 className="title">Telephone Number: {this.state.developer.telNumber}</h3>
+          <h1 className="title">Telephone Number: {this.state.developer.telNumber}</h1>
           <hr />
 
-          <h3 className="title">Email: {this.state.developer.email}</h3>
-        
-          <GoogleMap location={this.state.developer.location}/>
+          <h3 className="title">Email: <a href="mailto:somone@4horse-men.com">{this.state.developer.email}</a></h3>
+
+          <GoogleMap location={this.state.developer.location} />
+
+          <Footer />
 
         </div>
       </div>
+
+
     );
   }
 }
