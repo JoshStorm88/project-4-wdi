2238e5c1596901e034df88175798bab5a9c98c45
Josh Storm <joshua_taljaard@outlook.com>
2018-07-31 10:44:45 +0100

    changed eviroment issues

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const dbURI = 'mongodb://localhost:27017/codeshop';
-const port = 4000;
+const port = process.env.PORT || 4000;
 const secret = 'hellotheregeneralkenobi';
 
 module.exports = { dbURI, port, secret };
